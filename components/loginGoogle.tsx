d47cef51273b7d07a6c598e5f8da7fc8e4fe8f42
sebast825 <sebamolina825@gmail.com>
2025-12-22 10:08:52 -0300

    update supabase auth  import

--- components/loginGoogle.tsx
+++ components/loginGoogle.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { createClient } from "@supabase/supabase-js";
-
+import { createBrowserClient } from "@supabase/ssr";
 export default function LoginGoogle() {
   //init supabase client for the navigator
-  const supabase = createClient(
+  const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
