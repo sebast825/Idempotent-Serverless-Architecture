f9f0d33ccd429c3c42e3d3d9f367b8e9ef804573
sebast825 <sebamolina825@gmail.com>
2025-12-22 10:13:39 -0300

    middleware redirect home if not loged

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({ request })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll: () => request.cookies.getAll(),
+        setAll: (cookies) => cookies.forEach(({ name, value }) => request.cookies.set(name, value)),
+      },
+    }
+  )
+
+  const { data: { user } } = await supabase.auth.getUser()
+
+ //redirect if user is not loged
+  if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return response
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+}
