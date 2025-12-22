48d03434abb97bc1182abe203547acb8c81fe1bf
sebast825 <sebamolina825@gmail.com>
2025-12-22 11:04:25 -0300

    added logout, and helper to validate from server

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -2,17 +2,36 @@
 
 import { Nav, Navbar } from "react-bootstrap";
 import { useLogin } from "@/hooks/auth/useLogin";
+import { signOut } from "@/app/auth/actions";
+import { User } from "@supabase/supabase-js"; // Importa el tipo oficial
+import { useEffect } from "react";
 
-export const NavBar = () => {
+export const NavBar = ({ user }: { user: User | null }) => {
   const { loginWithGoogle, isLoading } = useLogin();
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="shadow">
       <Nav className="w-100 px-4 d-flex justify-content-end  justify-content-md-center flex-row gap-3 gap-md-5">
         <Nav.Link href="/">Home</Nav.Link>
 
-        <Nav.Link onClick={loginWithGoogle} disabled={isLoading}>
-          {isLoading ? "Cargando..." : "Login"}
-        </Nav.Link>
+        {!user ? (
+          <Nav.Link onClick={loginWithGoogle} disabled={isLoading}>
+            {isLoading ? "Cargando..." : "Login"}
+          </Nav.Link>
+        ) : (
+          <Nav.Link as="div">
+            <form action={signOut}>
+              <button
+                type="submit"
+                className="nav-link border-0 bg-transparent p-0 m-0"
+              >
+                Logout
+              </button>
+            </form>
+          </Nav.Link>
+        )}
       </Nav>
     </Navbar>
   );
