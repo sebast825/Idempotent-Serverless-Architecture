13556985d0dd61ba373bf7b952e6634a6d097bcf
sebast825 <sebamolina825@gmail.com>
2025-12-23 22:45:01 -0300

    feat more contrast to container

--- app/game/components/guessRow.tsx
+++ app/game/components/guessRow.tsx
@@ -8,29 +8,31 @@ interface propsGuessRow {
 export default function GuessRow(props: propsGuessRow) {
   const { currentGuess, handleRemoveColor } = props;
   return (
-    <div className="d-flex align-items-center justify-content-center w-100  gap-4 p-1 bg-slate-800 rounded-xl shadow-inner">
-      {currentGuess.map((color, index) => (
-        <button
-          key={index}
-          onClick={() => handleRemoveColor(index)}
-          className={`m-2 rounded-circle shadow-sm
+    <div className="d-flex justify-content-center p-1">
+      <div className="bg-dark p-3 px-4 rounded-5 ">
+        {currentGuess.map((color, index) => (
+          <button
+            key={index}
+            onClick={() => handleRemoveColor(index)}
+            className={`m-2 rounded-circle shadow-sm
             ${
               color
                 ? "scale-100 shadow-lg"
                 : "scale-90 opacity-50 border-dashed"
             }
           `}
-          style={{
-            backgroundColor: color?.toLowerCase() || "transparent",
-            width: "40px",
-            height: "40px",
-            display: "inline-block",
-            cursor: "pointer",
-            border: "2px solid #666",
-          }}
-          aria-label={`Select ${color}`}
-        ></button>
-      ))}
+            style={{
+              backgroundColor: color?.toLowerCase() || "transparent",
+              width: "40px",
+              height: "40px",
+              display: "inline-block",
+              cursor: "pointer",
+              border: "2px solid #ccc",
+            }}
+            aria-label={`Select ${color}`}
+          ></button>
+        ))}
+      </div>
     </div>
   );
 }
