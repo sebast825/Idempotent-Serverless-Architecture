2bf4142fc6b7c743ae572da6a1f615ced3a63796
sebast825 <sebamolina825@gmail.com>
2025-12-24 14:49:07 -0300

    added pagination hook and component

---
+++ hooks/usePagination.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const usePagination = () => {
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(5);
+
+  const goToPage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  return {
+    page,
+    pageSize,
+    goToPage   
+  };
+};
