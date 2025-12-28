aad2384b139ab10da9680789decea9ddf111f53b
sebast825 <sebamolina825@gmail.com>
2025-12-28 15:23:57 -0300

    feat - size modal lg - moved header outside body

--- components/modals/genericModal.tsx
+++ components/modals/genericModal.tsx
@@ -3,22 +3,20 @@ import { Modal, Button } from "react-bootstrap";
 interface ModalProps {
   onClose: () => void;
   children: React.ReactNode;
-  title : string
+  title: string;
 }
 
-export function GenericModal({ onClose, children,title }: ModalProps) {
+export function GenericModal({ onClose, children, title }: ModalProps) {
   return (
-    <Modal show={true} onHide={() => onClose()} centered scrollable>
-      <Modal.Body className="p-0">
-        <Modal.Header
-          closeButton
-          closeVariant="white"
-          className="bg-primary text-white"
-        >
-          <Modal.Title>{title}</Modal.Title>
-        </Modal.Header>
-        {children}
-      </Modal.Body>
+    <Modal show={true} onHide={() => onClose()} centered scrollable size="lg">
+      <Modal.Header
+        closeButton
+        closeVariant="white"
+        className="bg-primary text-white mb-2"
+      >
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="p-0">{children}</Modal.Body>
 
       <Modal.Footer>
         <Button variant="warning" onClick={() => onClose()}>
