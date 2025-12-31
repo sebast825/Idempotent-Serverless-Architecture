1c1e3303a85f1584e93a25b195a823952a9738f5
sebast825 <sebamolina825@gmail.com>
2025-12-31 16:07:50 -0300

    feat added header logo and redirect

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -10,6 +10,8 @@ import {
   useNotifications,
   useSetNoticationReadAt,
 } from "@/hooks/useNotifications";
+import { ROUTES } from "@/lib/routes";
+import Link from "next/link";
 
 export const NavBar = ({ user }: { user: User | null }) => {
   const openModal = useModalStore((state) => state.openModal);
@@ -27,64 +29,88 @@ export const NavBar = ({ user }: { user: User | null }) => {
   return (
     <>
       <Navbar bg="dark" variant="dark" fixed="top" className="shadow">
-        <Nav className="w-100 px-4 d-flex justify-content-end justify-content-md-center flex-row gap-3 gap-md-5 align-items-center">
-          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-
-          {!user ? (
-            <Nav.Link onClick={loginWithGoogle} disabled={isLoading}>
-              {isLoading ? "Cargando..." : "Login"}
-            </Nav.Link>
-          ) : (
-            <NavDropdown
-              title={
-                <span className="d-inline-flex align-items-center">
-                  {user.user_metadata?.full_name || "Mi Cuenta"}
-                  {notifications &&
-                    notifications?.some((elem) => elem.readAt == null) && (
-                      <Badge
-                        pill
-                        bg="danger"
-                        className="ms-2"
-                        style={{ fontSize: "0.6rem" }}
-                      >
-                        {
-                          notifications?.filter((elem) => elem.readAt == null)
-                            .length
-                        }
-                      </Badge>
-                    )}
-                </span>
-              }
-              id="user-nav-dropdown"
-              align="end"
-            >
-              <Nav.Link
-                onClick={() => handleOpenNotifications()}
-                className="dropdown-item py-1 px-3  text-dark"
+        <Nav
+          className="w-100 px-4 d-flex justify-content-end m-auto justify-content-md-between flex-row gap-3 gap-md-5 align-items-center px-5"
+          style={{ maxWidth: "1500px" }}
+        >
+          <div>
+            <Link href={user ? ROUTES.dashboard() : ROUTES.home()}>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#2ac4dfff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
-                Notifications
-              </Nav.Link>
+                <polygon points="7 4 21 12 7 20 7 4"></polygon>
+              </svg>
+            </Link>
+          </div>
+          <div className="d-flex">
+            {!user ? (
               <Nav.Link
-                onClick={() => openModal("HISTORYGAMES")}
-                className="dropdown-item py-1 px-3  text-dark"
-              >
-                My History
-              </Nav.Link>
-               <Nav.Link
-                onClick={() => openModal("USERSTATS")}
-                className="dropdown-item py-1 px-3  text-dark"
-              >
-                Performance
-              </Nav.Link>
-              <Nav.Link
-                onClick={() => signOut()}
+                as={Link}
+                onClick={loginWithGoogle}
                 disabled={isLoading}
-                className="dropdown-item py-1 px-3 text-danger"
               >
-                Logout
+                {isLoading ? "Cargando..." : "Login"}
               </Nav.Link>
-            </NavDropdown>
-          )}
+            ) : (
+              <NavDropdown
+                title={
+                  <span className="d-inline-flex align-items-center">
+                    {user.user_metadata?.full_name || "Mi Cuenta"}
+                    {notifications &&
+                      notifications?.some((elem) => elem.readAt == null) && (
+                        <Badge
+                          pill
+                          bg="danger"
+                          className="ms-2"
+                          style={{ fontSize: "0.6rem" }}
+                        >
+                          {
+                            notifications?.filter((elem) => elem.readAt == null)
+                              .length
+                          }
+                        </Badge>
+                      )}
+                  </span>
+                }
+                id="user-nav-dropdown"
+                align="end"
+              >
+                <Nav.Link
+                  onClick={() => handleOpenNotifications()}
+                  className="dropdown-item py-1 px-3  text-dark"
+                >
+                  Notifications
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => openModal("HISTORYGAMES")}
+                  className="dropdown-item py-1 px-3  text-dark"
+                >
+                  My History
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => openModal("USERSTATS")}
+                  className="dropdown-item py-1 px-3  text-dark"
+                >
+                  Performance
+                </Nav.Link>
+
+                <Nav.Link
+                  onClick={() => signOut()}
+                  disabled={isLoading}
+                  className="dropdown-item py-1 px-3 text-danger"
+                >
+                  Logout
+                </Nav.Link>
+              </NavDropdown>
+            )}
+          </div>
         </Nav>
       </Navbar>
     </>
--- lib/routes.ts
+++ lib/routes.ts
@@ -2,4 +2,6 @@ export const ROUTES = {
   game: (id: string) => `/games/${id}`,
   reviewGame: (id: string) => `/games/${id}/review`,
   not_found: () => "/404",
+  dashboard: () => "/dashboard",
+  home: () => "/",
 };
