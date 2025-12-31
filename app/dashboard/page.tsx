2ff72ba2063e03c9995ee516750623ce2d15538e
sebast825 <sebamolina825@gmail.com>
2025-12-31 17:52:11 -0300

    added container

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,14 +4,17 @@ import { Opening } from "@/components/opening";
 
 import { DashboardActions } from "./comonents/dashboardActions";
 import { Footer } from "@/components/footer";
+import { Container } from "react-bootstrap";
 
 export default function Dashboard() {
   return (
     <>
       <div className="d-flex flex-column  min-vh-100">
-        <div className="flex-grow-1 margin-top px-2 px-md-5  ">
-          <Opening />
-          <DashboardActions />
+        <div className="flex-grow-1 margin-top  ">
+          <Container>
+            <Opening />
+            <DashboardActions />
+          </Container>{" "}
         </div>
         <Footer />
       </div>
