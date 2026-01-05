e764b98447e7bb10b68139f676b32040e5a11b83
sebast825 <sebamolina825@gmail.com>
2026-01-04 21:41:21 -0300

    feat: show full width btn in mobile

--- app/home/components/heroActions.tsx
+++ app/home/components/heroActions.tsx
@@ -1,35 +1,38 @@
-"use client"
+"use client";
 import { useCreatePuzzleAndGame } from "@/hooks/game/useCreatePuzzleAndGame";
 import { useModalStore } from "@/store/useModalStore";
-import { Container, Stack, Button } from "react-bootstrap"
+import { Container, Stack, Button } from "react-bootstrap";
 
-export const HeroActions = () =>{
-     const { createPuzzleAndGame, isPending } = useCreatePuzzleAndGame();
+export const HeroActions = () => {
+  const { createPuzzleAndGame, isPending } = useCreatePuzzleAndGame();
   const openModal = useModalStore((state) => state.openModal);
-   return (
-  <Container className="text-center mt-4">
-          <Stack
-            direction="horizontal"
-            gap={3}
-            className="justify-content-center flex-wrap p-3"
-          >
-            <Button
-              variant="primary"
-              size="lg"
-              onClick={async () => await createPuzzleAndGame()}
-              disabled={isPending}
-            >
-              Play Demo
-            </Button>
+  return (
+    <Container className="text-center mt-4">
+      <Stack
+        direction="vertical"
+        gap={3}
+        className="justify-content-center  p-3 flex-sm-row m-auto"
+        style={{ maxWidth: "600px" }}
+      >
+        <Button
+          variant="primary"
+          className="w-100 w-sm-auto"
+          size="lg"
+          onClick={async () => await createPuzzleAndGame()}
+          disabled={isPending}
+        >
+          Play Demo
+        </Button>
 
-            <Button
-              variant="outline-secondary"
-              size="lg"
-              onClick={() => openModal("GAME_RULES")}
-            >
-              How It Works
-            </Button>
-          </Stack>
-        </Container>
-   )
-}
+        <Button
+          variant="outline-secondary"
+          className="w-100 w-sm-auto"
+          size="lg"
+          onClick={() => openModal("GAME_RULES")}
+        >
+          How It Works
+        </Button>
+      </Stack>
+    </Container>
+  );
+};
